2e69477e134068d32421bfce1dd476408397a7ae
yash.v <23f3000500@ds.study.iitm.ac.in>
2025-06-23 17:58:55 +0530

    REF: Refactor font usage in styles to utilize DM Sans and DM Mono

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { DM_Sans, DM_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmMono = DM_Mono({
+  variable: "--font-dm-mono",
   subsets: ["latin"],
+  weight: ["300", "400", "500"],
 });
 
 export const metadata: Metadata = {
@@ -25,9 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${dmSans.variable} ${dmMono.variable} antialiased`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start max-w-[500px]">
+      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start max-w-[500px] font-mono">
         <Image
           className="w-full"
           src="/logo-dark.svg"
-          alt="Next.js logo"
+          alt="Packright Solutions Logo"
           width={180}
           height={38}
           priority
@@ -15,7 +15,7 @@ export default function Home() {
         <p className={`font-mono tracking-tighter text-justify text-xl`}>
           All the right packaging solutions for your business at one place.
         </p>
-        <ul className="list-inside list-disc text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <ul className="list-inside list-disc text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">In development</li>
           <li className="tracking-[-.01em]">
             Contact: <br />
